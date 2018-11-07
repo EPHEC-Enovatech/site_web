fba27b9d9e80a44aaa8ede1fa026913f43394951
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-11-07 16:10:30 +0100

    Integrate authentication

---
+++ JS/auth.js
@@ -0,0 +1,84 @@
+
+function authenticate(evt) {
+    evt.preventDefault();
+    let form = $("#formConnexion")[0];
+    let data = { "auth": { "email": form.subMail.value, "password": form.password.value }};
+
+    fetch("https://api.sensorygarden.be/user_token", {
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(response => {
+        return response.json()
+    }).then(json => {
+        let payload = parseJWT(json.jwt)
+        setCookie('user-id', payload.sub, 1)
+        setCookie('token', json.jwt, 1)
+        window.location = "userPage.html"
+    }).catch(error => {
+        console.error(error)
+    })
+}
+
+function setCookie(name, value, exp_day) {
+    let exp = new Date()
+    exp.setTime(exp.getTime() + (exp_day*24*60*60*1000))
+    document.cookie = name + "=" + value + "; expires=" + exp.toUTCString() + ";"
+}
+
+function signin(evt) {
+    evt.preventDefault();
+    let data = {
+        nom: evt.target.subName.value,
+        prenom: evt.target.subSurname.value,
+        email: evt.target.subMail.value,
+        password: evt.target.password.value,
+        password_confirmation: evt.target.confirmPassword.value
+    };
+
+    fetch('https://api.sensorygarden.be/users/', {
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(json => {
+        if (json.status == "SUCCESS") {
+
+        } else {
+            
+        }
+    })
+}
+
+function getCookie(name) {
+    let cname = name + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cname) == 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function deleteCookie(name) {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+}
+
+function parseJWT(token) {
+    let base64Url = token.split('.')[1]
+    let base64 = base64Url.replace('-', '+').replace('_', '/')
+    return JSON.parse(window.atob(base64))
+}
--- JS/userPage.js
+++ JS/userPage.js
@@ -1,9 +1,10 @@
 function calculateSizeMain(){
     let tailleHeader = parseFloat((($('#document_Header').css('height')).split("px"))[0]);
-    let tailleFooter = parseFloat((($('#document_Footer').css('height')).split("px"))[0]);
-    let tailleHeaderFooter = tailleHeader + tailleFooter;
 
-    $('#document_Main').css('min-height', "calc(100vh - " + tailleHeaderFooter + "px)");
+    $('#document_Main').css('height', "calc(100vh - " + tailleHeader + "px)");
+    $('#menuUser').css('max-height', "calc(100vh - " + tailleHeader + "px)");
+    $('#contentZone').css('max-height', "calc(100vh - " + tailleHeader + "px)");
+    $('#sideNav').css('max-height', "calc(100vh - " + tailleHeader + "px)");
 }
 
 function loadHTML(page){
@@ -15,22 +16,89 @@ function loadHTML(page){
     } else {
         contentZone.load("userHTML/" + page + ".html", function( response, status, xhr ) {
             if (status === "error") {
-                contentZone.html("<p>Erreur, vérifier votre connexion internet</p>");
+                contentZone.html("<p>Impossible de charger le contenu, vérifier votre connexion internet</p>");
             }
         });
     }
 }
 
-$(function(){
-    calculateSizeMain();
+function toggleHoverMenu(li){
+    $('.sousMenu').removeClass('currentUserMenu');
+    $('.' + li.classList[1]).addClass('currentUserMenu');
+}
 
-    $('.sousMenu').on("click", function(){
-        loadHTML($(this).attr('id'));
+//Gère le menu latéral en repsonsive
+function toggleSlideMenu(){
+    if(window.innerWidth <= 600){
+        $('#menuUser').show().css("max-height", "100vh").css("height", "100vh");
+        $('#sideNav').css("height", window.innerHeight).css("max-height", window.innerHeight);
+        $('.menu-link').show().bigSlide({
+            menu: '#menuUser',
+            easyClose: true,
+        });
+    } else {
+        $('#sideNav').css("height", "auto");
+        $('#menuUser').css("position", "initial").css("left", 0).css("width", "auto").css("height", "auto");
+        $('.menu-link').hide();
+        calculateSizeMain();
+    }
+}
+
+$(function(){
+
+    moment.locale("fr");
+    $('.sousMenu').on("click", function(e){
+
+        if(e.target.hasAttribute("href")){
+            loadHTML(e.target.parentNode.id);
+            toggleHoverMenu(e.target.parentElement);
+        } else {
+            loadHTML($(this).attr('id'));
+            toggleHoverMenu(e.target);
+        }
     });
 
     $('#contentZone').load('userHTML/userInfo.html');
 
     $(window).on("resize", function(){
-        calculateSizeMain();
+        toggleSlideMenu();
     });
+
+    $("#logout").click((e) => {
+        e.preventDefault();
+        deleteCookie('token');
+        deleteCookie('user-id');
+        window.location = e.target.href;
+    });
+
+    callAPI("users/" + getCookie('user-id'), showUserInfo);
+
+    calculateSizeMain();
+    toggleSlideMenu();
 });
+
+function callAPI(arg, func){
+    $.ajax({
+        url: "https://api.sensorygarden.be/" + arg,
+        type: "GET",
+        beforeSend: function(xhr){xhr.setRequestHeader('Authorization', 'Bearer ' + getCookie('token'));},
+        complete: func
+    });
+}
+
+//Affiche les données personnelles de l'utilisateur
+function showUserInfo(response, textStatus) {
+    $('#userWelcome').html(response.responseJSON.data.prenom + " " + response.responseJSON.data.nom);
+    $('#userName').val(response.responseJSON.data.nom);
+    $('#userSurname').val(response.responseJSON.data.prenom);
+    $('#userMail').val(response.responseJSON.data.email);
+}
+
+//Affiche les box de l'utilisateur dans showBox
+function showBox(response){
+    let table = "<td>";
+    table += response.responseJSON.data.deviceName + "</td><td>";
+    table += response.responseJSON.data.device_id + "</td>";
+
+    $('#insertBox').html(table);
+}
