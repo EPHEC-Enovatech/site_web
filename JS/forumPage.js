0871fac053aecd9f2c45cac80b8ce9ff483db943
JonGoo <j.goossens@students.ephec.be>
2018-11-22 12:06:07 +0100

    Modification du filtre des publications, Modification du bouton de creation de post

--- JS/forumPage.js
+++ JS/forumPage.js
@@ -42,43 +42,36 @@ function listPublication(response) {
         let publication = response.responseJSON.data;
         let select = $('#filterCat').select2('data');
         let filter = [];
-        let isFiltrer = true;
         if(select.length != 0) {
             for (let k in select) {
                 filter.push(parseInt(select[k].id))
             }
+            publication = publication.filter(function(x) {
+                return x.categories.filter(function(y) {
+                    return filter.includes(y.id)
+                }).length
+            })
         }
         for(item in publication){
             let arrCategories = publication[item].categories;
-            if(filter.length!=0) {
-                let bool = false;
-                for (let k=0; k<arrCategories.length && bool ==false; k++) {
-                    if (filter.includes(arrCategories[k].id)) {
-                        isFiltrer = true;
-                        bool=true;
-                    } else {
-                        isFiltrer = false;
+
+            list += "<li> <div class=\"auteur\"> <h3 class=\"name\"><bdi>" + "" + "</bdi></h3>";
+            list += "<img src=\"IMG/avatar.png\" alt=\"avatar image\" class=\"avatar\"></div>";
+            list += "<div class=\"publication\"> <h3 class=\"object\">" + publication[item].postTitle + "</h3>";
+            list += "<p class=\"message\">" + publication[item].postText.substring(0, 200) + "...</p>";
+            list += "<select class=\"cat catPubli\" name=\"states[]\" multiple=\"multiple\" disabled>";
+            for (cat in categories) {
+                for (i in arrCategories) {
+                    if (arrCategories[i].id == categories[cat].id) {
+                        list += "<option value=\"" + categories[cat].id + "\" selected>" + categories[cat].categoryName + "</option>";
                     }
                 }
             }
-            if(isFiltrer) {
-                list += "<li> <div class=\"auteur\"> <h3 class=\"name\"><bdi>" + "" + "</bdi></h3>";
-                list += "<img src=\"IMG/avatar.png\" alt=\"avatar image\" class=\"avatar\"></div>";
-                list += "<div class=\"publication\"> <h3 class=\"object\">" + publication[item].postTitle + "</h3>";
-                list += "<p class=\"message\">" + publication[item].postText.substring(0, 200) + "...</p>";
-                list += "<select class=\"cat catPubli\" name=\"states[]\" multiple=\"multiple\" disabled>";
-                for (cat in categories) {
-                    for (i in arrCategories) {
-                        if (arrCategories[i].id == categories[cat].id) {
-                            list += "<option value=\"" + categories[cat].id + "\" selected>" + categories[cat].categoryName + "</option>";
-                        }
-                    }
-                }
-                list += "</select>";
-                let date = moment(new Date(publication[item].postDate)).format("LLL");
-                list += "<p class=\"date\">" + date + "</p>";
-                list += "</div></li>";
-            }
+            list += "</select>";
+            let date = moment(new Date(publication[item].postDate)).format("LLL");
+            list += "<p class=\"date\">" + date + "</p>";
+            list += "</div></li>";
+
         }
     } else {
         list += "<li>Il n'y a pas de publication disponible pour l'instant</li>";
