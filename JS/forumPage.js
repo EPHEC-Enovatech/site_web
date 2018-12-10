f716d28212ab6d1d474dc5536b7b9e8daac85345
simon-fauconnier <simon.fauconnier@gmail.com>
2018-12-10 21:58:37 +0100

    Synchronisation avec le dev

--- JS/forumPage.js
+++ JS/forumPage.js
@@ -4,50 +4,47 @@ $(document).ready(function() {
         placeholder: "Choisissez une ou plusieurs catégories à filtrer",
 
         width: '49vw',
-        allowClear: true,
+        allowClear: !0,
         dropdownParent: $('#dropdown')
+    }).on('change', function(e) {
+        callAPI("posts", listPublication)
     });
     callAPI("categories", selectCategorie);
     callAPI("posts", listPublication);
-    $('#filterCat').on('change', function (e) {
-        callAPI("posts", listPublication);
+    $(window).on("resize", function() {
+        toggleSlideMenu()
     });
-    $(window).on("resize", function(){
-        toggleSlideMenu();
-    });
-    $('#mobile_menu_button_side').on("click", function(){
-        toggleSlideMenu();
+    $('#mobile_menu_button_side').on("click", function() {
+        toggleSlideMenu()
     });
     calculateSizeMain();
     toggleSlideMenu();
-
-
-
 });
+function setLoading(n) {
+
+}
 
 function selectCategorie(response) {
     let select = '';
     if (response.responseJSON.status === "SUCCESS") {
-
         categories = response.responseJSON.data;
-
-        for(item in categories){
+        for (item in categories) {
             select += "<option value=" + categories[item].id + ">";
-            select += categories[item].categoryName + "</option>";
+            select += categories[item].categoryName + "</option>"
         }
     } else {
-        select += "<option value=''>Aucunes catégories disponible</option>";
+        select += "<option value=''>Aucunes catégories disponible</option>"
     }
-    $('#filterCat').html(select);
+    $('#filterCat').html(select)
 }
+
 function listPublication(response) {
     let list = '';
     if (response.responseJSON.status === "SUCCESS") {
-
         let publication = response.responseJSON.data;
         let select = $('#filterCat').select2('data');
         let filter = [];
-        if(select.length != 0) {
+        if (select.length != 0) {
             for (let k in select) {
                 filter.push(parseInt(select[k].id))
             }
@@ -57,10 +54,9 @@ function listPublication(response) {
                 }).length
             })
         }
-        for(item in publication){
+        for (item in publication) {
             let arrCategories = publication[item].categories;
-
-            list += "<li id='"+publication[item].id+"'><div class=\"auteur\"> <h3 class=\"name\"><bdi>" + publication[item].user["prenom"] + "</bdi></h3>";
+            list += "<li id='" + publication[item].id + "'><div class=\"auteur\"> <h3 class=\"name\"><bdi>" + publication[item].user.prenom + "</bdi></h3>";
             list += "<img src=\"IMG/avatar.png\" alt=\"avatar image\" class=\"avatar\"></div>";
             list += "<div class=\"publication\"> <h3 class=\"object\">" + publication[item].postTitle + "</h3>";
             list += "<p class=\"message\">" + publication[item].postText.substring(0, 200) + "...</p>";
@@ -68,56 +64,54 @@ function listPublication(response) {
             for (cat in categories) {
                 for (i in arrCategories) {
                     if (arrCategories[i].id == categories[cat].id) {
-                        list += "<option value=\"" + categories[cat].id + "\" selected>" + categories[cat].categoryName + "</option>";
+                        list += "<option value=\"" + categories[cat].id + "\" selected>" + categories[cat].categoryName + "</option>"
                     }
                 }
             }
             list += "</select>";
             let date = moment(new Date(publication[item].postDate)).format("LLL");
             list += "<p class=\"date\">" + date + "</p>";
-            list += "</div></li>";
-
+            list += "</div></li>"
         }
     } else {
-        list += "<li>Il n'y a pas de publication disponible pour l'instant</li>";
+        list += "<li>Il n'y a pas de publication disponible pour l'instant</li>"
     }
-    if(list == '') list += "<li>Il n'y a pas de publication disponible pour l'instant</li>";
+    if (list == '') list += "<li>Il n'y a pas de publication disponible pour l'instant</li>";
     $('.listPubli').html(list);
-    $(function(){
-
+    $(function() {
         $("div.holder").jPages({
-            containerID : "paginationList",
+            containerID: "paginationList",
             perPage: 2,
             previous: "◄",
             next: "►"
-        });
-
+        })
     });
     $('.catPubli').select2();
-    $('.listPubli > li').on('click', function () {
-        window.location.href = "showPost.html?post_id="+$(this).attr('id');
+    $('.listPubli > li').on('click', function() {
+        window.location.href = "showPost.html?post_id=" + $(this).attr('id')
     })
 }
-function toggleSlideMenu(){
-    if(window.innerWidth <= 600){
+
+function toggleSlideMenu() {
+    if (window.innerWidth <= 600) {
         $('#menuUser').show().css("max-height", "100vh").css("height", "100vh");
         $('#sideNav').css("height", window.innerHeight).css("max-height", window.innerHeight);
         $('.menu-link').show().bigSlide({
             menu: '#menuUser',
-            easyClose: true,
-        });
+            easyClose: !0,
+        })
     } else {
         $('#sideNav').css("height", "auto");
         $('#menuUser').css("position", "initial").css("left", 0).css("width", "auto").css("height", "auto");
         $('.menu-link').hide();
-        calculateSizeMain();
+        calculateSizeMain()
     }
 }
-function calculateSizeMain(){
-    let tailleHeader = parseFloat((($('#document_Header').css('height')).split("px"))[0]);
 
+function calculateSizeMain() {
+    let tailleHeader = parseFloat((($('#document_Header').css('height')).split("px"))[0]);
     $('#document_Main').css('height', "calc(100vh - " + tailleHeader + "px)");
     $('#menuUser').css('max-height', "calc(100vh - " + tailleHeader + "px)");
     $('#contentZone').css('max-height', "calc(100vh - " + tailleHeader + "px)");
-    $('#sideNav').css('max-height', "calc(100vh - " + tailleHeader + "px)");
+    $('#sideNav').css('max-height', "calc(100vh - " + tailleHeader + "px)")
 }
