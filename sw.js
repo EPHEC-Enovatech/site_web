0743f32ded5aa44bbd16e2675448f58a0cfc51d0
Adrien Nini Pereira <adrien97nini@gmail.com>
2018-12-11 15:30:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
-var cache = "sensory-static-v2";
-var userCache = 'sensory-static-userdata-v2';
-var dataCache = "sensory-static-records-v2";
+var cache = "sensory-static-v3";
+var userCache = 'sensory-static-userdata-v3';
+var dataCache = "sensory-static-records-v3";
 
 var cachePages = [
     '/staging/',
