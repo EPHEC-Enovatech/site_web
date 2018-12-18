945f7b6fb18b21dc48ddc3519f6908e8380c1fff
Simon Fauconnier <32515627+simon-fauconnier@users.noreply.github.com>
2018-12-18 09:14:03 +0100

    version 10

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
-var cache = "sensory-static-v9";
-var userCache = 'sensory-static-userdata-v9';
-var dataCache = "sensory-static-records-v9";
+var cache = "sensory-static-v10";
+var userCache = 'sensory-static-userdata-v10';
+var dataCache = "sensory-static-records-v10";
 
 var cachePages = [
     '/',
